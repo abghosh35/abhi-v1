616dfd6b18ebec6e8e50d6ba85e5a2313f57984d
Abhijit Ghosh <10430166+abghosh35@users.noreply.github.com>
2020-03-07 12:46:17 +0530

    Update trainiterator.py

--- abhi/abhi/torch/train/trainiterator.py
+++ abhi/abhi/torch/train/trainiterator.py
@@ -44,7 +44,7 @@ class TrainIterator:
         self.batch_size = len(self.train_loader)
         
         if use_gpu:
-            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         else:
             self.device = torch.device('cpu')
             
